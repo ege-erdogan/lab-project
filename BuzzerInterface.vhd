b8ff6644f350498c0be3d1bb389738e752b52c50
egeerdogan <ege@egeerdogan.com>
2019-05-13 22:58:12 +0300

    initial commit

---
+++ BuzzerInterface.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    22:42:51 05/13/2019 
+-- Design Name: 
+-- Module Name:    BuzzerInterface - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity BuzzerInterface is
+end BuzzerInterface;
+
+architecture Behavioral of BuzzerInterface is
+
+begin
+
+
+end Behavioral;
+
