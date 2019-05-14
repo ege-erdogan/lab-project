514d503cbfa6d0d5587a6d01e653f8deac917d14
unknown <canardaaydin@gmail.com>
2019-05-14 12:58:01 +0300

    morse decoder

--- MorseDecoder.vhd
+++ MorseDecoder.vhd
@@ -30,12 +30,72 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity MorseDecoder is
+	port (
+		bit5Rep: in STD_LOGIC_VECTOR(4 downto 0);
+		bitRepOfMorse : out STD_LOGIC_VECTOR(13 downto 0)
+	);
 end MorseDecoder;
 
 architecture Behavioral of MorseDecoder is
 
 begin
 
+	case bit5Rep is
+			when "00000" => 
+				bitRepOfMorse <= "00000000000000"; --space
+			when "00001" => 
+				bitRepOfMorse <= "10111000000000"; --A
+			when "00010" => 
+				bitRepOfMorse <= "11101010100000"; --B
+			when "00011" =>	
+				bitRepOfMorse <= "11101011101000"; --C
+			when "00100" => 
+				bitRepOfMorse <= "11101010100000"; --D
+			when "00101" => 
+				bitRepOfMorse <= "10000000000000"; --E
+			when "00110" => 
+				bitRepOfMorse <= "10101110100000"; --F
+			when "00111" => 
+				bitRepOfMorse <= "11101110100000"; --G
+			when "01000" => 
+				bitRepOfMorse <= "10101010000000"; --H
+			when "01001" => 
+				bitRepOfMorse <= "10100000000000"; --I
+			when "01010" =>
+				bitRepOfMorse <= "10111011101110"; --J
+			when "01011" => 
+				bitRepOfMorse <= "11101011100000"; --K
+			when "01100" => 
+				bitRepOfMorse <= "10111010100000"; --L
+			when "01101" => 
+				bitRepOfMorse <= "11101110000000"; --M
+			when "01110" => 
+				bitRepOfMorse <= "11101000000000"; --N
+			when "01111" => 
+				bitRepOfMorse <= "11101110111000"; --O
+			when "10000" => 
+				bitRepOfMorse <= "10111011101000"; --P	
+			when "10001" => 
+				bitRepOfMorse <= "11101110101110"; --Q
+			when "10010" => 
+				bitRepOfMorse <= "10111010000000"; --R
+			when "10011" => 
+				bitRepOfMorse <= "10101000000000"; --S
+			when "10100" => 
+				bitRepOfMorse <= "11100000000000"; --T
+			when "10001" => 
+				bitRepOfMorse <= "10101110000000"; --U
+			when "10110" => 
+				bitRepOfMorse <= "10101011100000"; --V
+			when "10111" => 
+				bitRepOfMorse <= "10111011100000"; --W
+			when "11000" => 
+				bitRepOfMorse <= "11101010111000"; --X
+			when "11001" => 
+				bitRepOfMorse <= "11101011101110"; --Y
+			when "11010" => 
+				bitRepOfMorse <= "11101110101000"; --Z							
+		end case;
 
 end Behavioral;
 
